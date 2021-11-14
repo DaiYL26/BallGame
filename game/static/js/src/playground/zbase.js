c66c46147ec167273f1a026463951e27c82b40fc
D 26 <877669110@qq.com>
2021-11-14 18:40:47 +0800

    django lesson 4

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -8,10 +8,19 @@ class AcGamePlayground {
         this.height = this.$playground.height();
         // console.log(this.width, this.height);
         this.game_map = new GameMap(this);
-        this.playes = []
-        this.playes.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true))
+        this.players = []
+        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true))
         
+        for (let i = 0 ; i < 5; i ++) {
+            this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, this.get_random_color(), this.height * 0.15, false))
+        }
+
         this.start();
+    }
+
+    get_random_color() {
+        let colors = ["skyblue", "yellow", "pink", "grey", "green"];
+        return colors[Math.floor(Math.random() * 5)];
     }
 
     start() {
